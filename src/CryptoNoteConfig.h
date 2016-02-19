cab49f56fa6d128fef619f5bfbc001f693076799
Azelo <ozilem.j@gmail.com>
2016-02-20 01:08:24 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -4,3 +4,7 @@ const unsigned EMISSION_SPEED_FACTOR = 21;
 const uint64_t DIFFICULTY_TARGET = 160;
 const int P2P_DEFAULT_PORT = 32778;
 const int RPC_DEFAULT_PORT = 32779;
+const std::initializer_list<const char*> SEED_NODES = {
+  "199.51.51.61:12568",
+  "216.12.21.88:18765",
+};
