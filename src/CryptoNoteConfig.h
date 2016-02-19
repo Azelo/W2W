7e06319877f189ad9e8de0e14165dcffe6be7290
Azelo <ozilem.j@gmail.com>
2016-02-20 01:00:54 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,4 +1,4 @@
 const char CRYPTONOTE_NAME[] = "Azalo";
 const uint64_t MONEY_SUPPLY = (uint64_t)(-1);
-const unsigned EMISSION_SPEED_FACTOR = 18;
-const uint64_t DIFFICULTY_TARGET = 150;
+const unsigned EMISSION_SPEED_FACTOR = 21;
+const uint64_t DIFFICULTY_TARGET = 160;
