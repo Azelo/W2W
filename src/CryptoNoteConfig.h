6c472845d1faf033c80dc91857536c1a8dfae0b4
Azelo <ozilem.j@gmail.com>
2016-02-20 01:05:49 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -2,3 +2,5 @@ const char CRYPTONOTE_NAME[] = "Azalo";
 const uint64_t MONEY_SUPPLY = (uint64_t)(-1);
 const unsigned EMISSION_SPEED_FACTOR = 21;
 const uint64_t DIFFICULTY_TARGET = 160;
+const int P2P_DEFAULT_PORT = 32778;
+const int RPC_DEFAULT_PORT = 32779;
