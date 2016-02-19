d8b2cb61c894e5727eadc59ab133c33f0cba0983
Azelo <ozilem.j@gmail.com>
2016-02-20 01:13:15 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -8,3 +8,7 @@ const std::initializer_list<const char*> SEED_NODES = {
   "199.51.51.61:12568",
   "216.12.21.88:18765",
 };
+const uint64_t MINIMUM_FEE = 1000000;
+const size_t CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE = 20000
+const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 0x1224c8; // addresses start with "aasw"
+const char GENESIS_COINBASE_TX_HEX[] = "";
