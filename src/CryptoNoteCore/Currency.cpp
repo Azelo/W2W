125d853b3f51b75ccaeda70eb1f888e62cee1146
Azelo <ozilem.j@gmail.com>
2016-02-20 01:00:09 +0200

    Update Currency.cpp

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -1,2 +1,3 @@
 bool Currency::getBlockReward(size_t medianSize, size_t currentBlockSize, uint64_t alreadyGeneratedCoins, 
 uint64_t fee, uint64_t& reward, int64_t& emissionChange) const
+uint64_t baseReward = (m_moneySupply - alreadyGeneratedCoins) >> m_emissionSpeedFactor;
